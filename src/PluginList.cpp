8a1d48c97f57fe5eb49b6e53b8773713929730e8
Chien-Liang Fok <liangfok@gmail.com>
2014-09-24 09:08:07 -0700

    Added .gitignore. Updated CMakeLists.txt to generate library. Added XML file describing plugins. Fixed bug in manifest. Updated to reference ROS Indigo.

--- src/PluginList.cpp
+++ src/PluginList.cpp
@@ -3,7 +3,7 @@
 
 #include <controlit/servo_clock_library/ServoClockDreamer.hpp>
 
-// Defined in /opt/ros/hydro/include/pluginlib/class_list_macros.h:
+// Defined in /opt/ros/indigo/include/pluginlib/class_list_macros.h:
 //
 PLUGINLIB_EXPORT_CLASS(controlit::servo_clock_library::ServoClockDreamer, controlit::ServoClock);
 
