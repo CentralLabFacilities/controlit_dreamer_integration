9a01d08372be39ee319a6ef909bafd3db5e9f02d
Chien-Liang Fok <liangfok@gmail.com>
2014-12-22 17:45:51 -0800

    Finished implementing and testing JointPositionErrorDisplayer.py.

--- nodes/JointPositionErrorDisplayer.py
+++ nodes/JointPositionErrorDisplayer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+# import string
 
 import rospy
 from std_msgs.msg import Float64MultiArray
@@ -35,8 +36,26 @@ if __name__ == '__main__':
     rospy.wait_for_service('/dreamer_controller/diagnostics/getJointIndices')
     try:
         get_joint_indices_service = rospy.ServiceProxy('/dreamer_controller/diagnostics/getJointIndices', get_parameters)
-        resp1 = get_joint_indices_service()
-        message = "Received joint indices: {0}".format(resp1)
+        joint_indices_msg = get_joint_indices_service()
+        message = "Received joint indices: {0}".format(joint_indices_msg)
+        rospy.loginfo(message)
+
+        resultList = [["Index", "Joint Name", "Error (rad)", "Error (deg)"]]
+        resultList.append(["-----", "----------", "-----------", "-----------"])
+        for joint in joint_indices_msg.params.values:
+            # rospy.loginfo("{0} {1} {2}".format(joint.key, joint.value, jointErrorMessage.data[int(joint.key)]))
+            errorRad = jointErrorMessage.data[int(joint.key)]
+            errorDeg = errorRad / 3.14 * 180
+            entry = [str(joint.key), joint.value, str(errorRad), str(errorDeg)]
+            resultList.append(entry)
+        
+        col_width = max(len(word) for row in resultList for word in row) + 2  # padding
+        resultTable = "\n"
+        for row in resultList:
+            resultTable = resultTable + "".join(word.ljust(col_width) for word in row)
+            resultTable = resultTable + "\n"
+        rospy.loginfo(resultTable)
+
     except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+        rospy.logerror("Service call failed: %s"%e)
 
