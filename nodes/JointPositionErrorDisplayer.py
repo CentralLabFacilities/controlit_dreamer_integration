761034fefa4a0d859a0a2b9c39347ae7596b564f
Chien-Liang Fok <liangfok@utexas.edu>
2015-01-06 09:35:38 -0600

    Changed name of service.

--- nodes/JointPositionErrorDisplayer.py
+++ nodes/JointPositionErrorDisplayer.py
@@ -6,7 +6,9 @@ import rospy
 from std_msgs.msg import Float64MultiArray
 from controlit_core.srv import get_parameters
 
-jointErrorMessage = 0
+JOINT_INDICES_SERVICE = "/dreamer_controller/diagnostics/getRealJointIndices"
+
+jointErrorMessage = None
 
 def jointErrorCallback(data):
     global jointErrorMessage
@@ -20,22 +22,24 @@ if __name__ == '__main__':
 
     subscriber = rospy.Subscriber("/dreamer_controller/JPosTask/error", Float64MultiArray, jointErrorCallback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    # rospy.spin()
-
-    # rospy.loginfo("Waiting for joint error message to arrive...")
-    while jointErrorMessage == 0:
-        time.sleep(.01)
-
-    # message = "Received joint error message:\n{0}".format(jointErrorMessage)
-    # rospy.loginfo(message)
+    # Wait for the current Cartesian position and orientation measurements to arrive
+    pauseCount = 0
+    printWarning = False
+    while not rospy.is_shutdown() and jointErrorMessage == None:
+        time.sleep(0.5)
+        pauseCount = pauseCount + 1
+        if pauseCount > 5 and not printWarning:
+            print "Waiting for joint error information..."
+            printWarning = True
 
     subscriber.unregister()
 
     # Call the service that returns the joint indices
-    rospy.wait_for_service('/dreamer_controller/diagnostics/getJointIndices')
+    print "Waiting for service {0}...".format(JOINT_INDICES_SERVICE)
+    rospy.wait_for_service(JOINT_INDICES_SERVICE)
     try:
-        get_joint_indices_service = rospy.ServiceProxy('/dreamer_controller/diagnostics/getJointIndices', get_parameters)
+        print "Calling service {0}...".format(JOINT_INDICES_SERVICE)
+        get_joint_indices_service = rospy.ServiceProxy(JOINT_INDICES_SERVICE, get_parameters)
         joint_indices_msg = get_joint_indices_service()
         # message = "Received joint indices: {0}".format(joint_indices_msg)
         # rospy.loginfo(message)
@@ -57,5 +61,5 @@ if __name__ == '__main__':
         rospy.loginfo("Results:\n{0}".format(resultTable))
 
     except rospy.ServiceException, e:
-        rospy.logerror("Service call failed: %s"%e)
+        rospy.logerr("Service call failed: %s"%e)
 
