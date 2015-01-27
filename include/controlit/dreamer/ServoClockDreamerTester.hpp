5a688b54bc84175bedda24d5cc4a292edf090d6f
Chien-Liang <liangfok@utexas.edu>
2015-01-27 09:45:07 -0600

    Removed all copyright notices.

--- include/controlit/dreamer/ServoClockDreamerTester.hpp
+++ include/controlit/dreamer/ServoClockDreamerTester.hpp
@@ -1,26 +1,3 @@
-/*
- * Shared copyright notice and LGPLv3 license statement.
- *
- * Copyright (C) 2014 University of Texas at Austin. All rights reserved.
- *
- * Authors: Chien-Liang Fok (UT Austin)
- *
- * Website: http://www.me.utexas.edu/~hcrl/
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/>
- */
 #ifndef __CONTROLIT_SERVO_CLOCK_DREAMER_TESTER_HPP__
 #define __CONTROLIT_SERVO_CLOCK_DREAMER_TESTER_HPP__
 
